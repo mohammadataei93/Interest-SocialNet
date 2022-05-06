15f6d31bf761a6e37e8ef56838170a2a027497c3
mohammad.ataei <mohammad.ataei93@yahoo.com>
2022-05-06 15:20:10 +0430

    initial commit

---
+++ actions/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class Action(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name='actions',
+                             db_index=True,
+                             on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True,
+                                   db_index=True)
+    target_ct = models.ForeignKey(ContentType,
+                                  related_name='target_obj',
+                                  on_delete=models.CASCADE,
+                                  blank=True,
+                                  null=True)
+    target_id = models.PositiveIntegerField(blank=True,
+                                            null=True,
+                                            db_index=True)
+    target = GenericForeignKey('target_ct', 'target_id')
+
+
+    class Meta:
+        ordering = ('-created',)
---
+++ images/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ImageCreateForm
+from django.contrib import messages
+from .models import Image
+from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+from common.decorators import ajax_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
+from actions.utils import create_action
+import redis
+from django.conf import settings
+
+
+
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
+            messages.success(request, 'Image added successfully!')
+            return redirect(new_item.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)
+        return render(request, 'images/image/create.html',
+                      {'section': 'images', 'form': form})
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f'image:{image.id}:views')
+    return render(request, 'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image,
+                   'total_views': total_views})
+
+@ajax_required
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+
+    return JsonResponse({'status': 'error'})
+
+@login_required
+def image_list(request):
+    images = Image.objects.order_by('-total_likes')
+    paginator = Paginator(images, 8)
+    page = request.GET.get("page")
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return render(request, 'images/image/list_ajax.html',
+                      {'section': 'images', 'images': images})
+
+    return render(request, 'images/image/list.html',
+                      {'section': 'images', 'images': images})
